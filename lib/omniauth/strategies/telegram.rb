872457e4a4c549b745fb194d4661152563fd3787
Peter Schröder <phoetmail@googlemail.com>
2021-03-15 17:09:26 +0100

    fix the occasional missing-param error

--- lib/omniauth/strategies/telegram.rb
+++ lib/omniauth/strategies/telegram.rb
@@ -6,17 +6,17 @@ module OmniAuth
   module Strategies
     class Telegram
       include OmniAuth::Strategy
-      
+
       args [:bot_name, :bot_secret]
-      
+
       option :name, 'telegram'
       option :bot_name, nil
       option :bot_secret, nil
       option :button_config, {}
-      
-      FIELDS      = %w[id first_name last_name username photo_url auth_date hash]
-      HASH_FIELDS = %w[auth_date first_name id last_name photo_url username]
-      
+
+      REQUIRED_FIELDS = %w[id hash]
+      HASH_FIELDS     = %w[auth_date first_name id last_name photo_url username]
+
       def request_phase
         html = <<-HTML
           <!DOCTYPE html>
@@ -27,23 +27,23 @@ module OmniAuth
           </head>
           <body>
         HTML
-        
+
         data_attrs = options.button_config.map { |k,v| "data-#{k}=\"#{v}\"" }.join(" ")
-        
+
         html << "<script async
               src=\"https://telegram.org/js/telegram-widget.js?4\"
               data-telegram-login=\"#{options.bot_name}\"
               data-auth-url=\"#{callback_url}\"
         #{data_attrs}></script>"
-        
+
         html << <<-HTML
           </body>
           </html>
         HTML
-        
+
         Rack::Response.new(html, 200, 'content-type' => 'text/html').finish
       end
-      
+
       def callback_phase
         if error = check_errors
           fail!(error)
@@ -51,11 +51,11 @@ module OmniAuth
           super
         end
       end
-      
+
       uid do
         request.params["id"]
       end
-      
+
       info do
         {
             name:       "#{request.params["first_name"]} #{request.params["last_name"]}",
@@ -65,35 +65,37 @@ module OmniAuth
             image:      request.params["photo_url"]
         }
       end
-      
+
       extra do
         {
             auth_date: Time.at(request.params["auth_date"].to_i)
         }
       end
-      
+
       private
 
       def check_errors
-        return :field_missing unless check_fields
+        return :field_missing unless check_required_fields
         return :signature_mismatch unless check_signature
         return :session_expired unless check_session
       end
 
-      def check_fields
-        FIELDS.all? { |f| request.params.include?(f) }
+      def check_required_fields
+        REQUIRED_FIELDS.all? { |f| request.params.include?(f) }
       end
-      
+
       def check_signature
-        secret = OpenSSL::Digest::SHA256.digest(options[:bot_secret])
-        signature = HASH_FIELDS.map { |f| "%s=%s" % [f, request.params[f]] }.join("\n")
-        hashed_signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, secret, signature)
-        
-        request.params["hash"] == hashed_signature
+        request.params["hash"] == self.class.calculate_signature(options[:bot_secret], request.params)
       end
 
       def check_session
         Time.now.to_i - request.params["auth_date"].to_i <= 86400
+      end
+
+      def self.calculate_signature(secret, params)
+        secret = OpenSSL::Digest::SHA256.digest(secret)
+        signature = HASH_FIELDS.map { |field| "%s=%s" % [field, params[field]] }.join("\n")
+        OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, secret, signature)
       end
     end
   end
--- spec/omniauth/telegram_spec.rb
+++ spec/omniauth/telegram_spec.rb
@@ -1,5 +1,53 @@
 RSpec.describe Omniauth::Telegram do
-  it "has a version number" do
-    expect(Omniauth::Telegram::VERSION).not_to be nil
+  def make_env(path = '/auth/test', props = {})
+    {
+      'REQUEST_METHOD' => 'POST',
+      'PATH_INFO' => path,
+      'rack.session' => {},
+      'rack.input' => StringIO.new('test=true')
+    }.merge(props)
+  end
+
+  before do
+  end
+
+  it 'fails with field_missing' do
+    env = make_env('/auth/telegram/callback')
+    app = lambda { |_env| [200, env, ['Telegram']] }
+    strategy = OmniAuth::Strategies::Telegram.new(app)
+    response = strategy.call!(env)
+
+    expect(response).to eq([302, {"Location"=>"/auth/failure?message=field_missing&strategy=telegram"}, ["302 Moved"]])
+  end
+
+  it 'fails with session_expired' do
+    params = {
+      'id' => '123',
+    }
+    hash = OmniAuth::Strategies::Telegram.calculate_signature('some-secret', params)
+    env = make_env('/auth/telegram/callback', 'rack.input' => StringIO.new(params.merge('hash' => hash).map {|k, v| "#{k}=#{v}" }.join('&')))
+    app = lambda { |_env| [200, env, ['Telegram']] }
+    strategy = OmniAuth::Strategies::Telegram.new(app)
+    strategy.options.bot_secret = 'some-secret'
+    strategy.options.bot_name = 'some-name'
+    response = strategy.call!(env)
+
+    expect(response).to eq([302, {"Location"=>"/auth/failure?message=session_expired&strategy=telegram"}, ["302 Moved"]])
+  end
+
+  it 'works' do
+    params = {
+      'id' => '123',
+      'auth_date' => Time.now.to_i,
+    }
+    hash = OmniAuth::Strategies::Telegram.calculate_signature('some-secret', params)
+    env = make_env('/auth/telegram/callback', 'rack.input' => StringIO.new(params.merge('hash' => hash).map {|k, v| "#{k}=#{v}" }.join('&')))
+    app = lambda { |_env| [200, env, ['Telegram']] }
+    strategy = OmniAuth::Strategies::Telegram.new(app)
+    strategy.options.bot_secret = 'some-secret'
+    strategy.options.bot_name = 'some-name'
+    response = strategy.call!(env)
+
+    expect(response.first).to eq(200)
   end
 end
